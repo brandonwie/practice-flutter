7f0738395715f80318f95602a384705468ed14e2
Brandon Wie <brandonwie.cs@gmail.com>
2023-01-24 13:23:29 +0900

    Create FloatingActionButton

--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,34 @@
 import 'package:flutter/material.dart';
 
 // Define a 'main' function to run when our app starts
-void main() {
-// Create a new text widget to show some text on the screen
-  const app = MaterialApp(home: Text('Hi My App! insnace manace'));
-// Take that widget and get it on the screen
-
-  runApp(app);
-}
-
-// void main() => runApp(MyApp());
+// void main() {
+// // Create a new text widget to show some text on the screen
+//   const app = MaterialApp(home: Text('Hi My App! insnace manace'));
+// // Take that widget and get it on the screen
 //
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(title: Text('Hot reload works let saldk!!')),
-//       ),
-//     );
-//   }
+//   runApp(app);
 // }
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.blueGrey,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Hot reload works!')),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              print('Hi there');
+            },
+            backgroundColor: Colors.greenAccent[400],
+            child: const Icon(Icons.add)),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      ),
+    );
+  }
+}
