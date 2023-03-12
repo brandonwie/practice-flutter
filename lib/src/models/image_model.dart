26f24ad214239f22587f2f6f132e4bf177b931f9
Brandon Wie <brandonwie.cs@gmail.com>
2023-03-12 19:48:36 +0900

    add: field type

--- lib/src/models/image_model.dart
+++ lib/src/models/image_model.dart
@@ -5,7 +5,7 @@ class ImageModel {
 
   ImageModel(this.id, this.url, this.title);
 
-  ImageModel.fromJson(parsedJson) {
+  ImageModel.fromJson(Map<String, dynamic> parsedJson) {
     id = parsedJson['id'];
     url = parsedJson['url'];
     title = parsedJson['title'];
