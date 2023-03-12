390c8673c4fa17c668ab9bee8033277053b809ab
Brandon Wie <brandonwie.cs@gmail.com>
2023-03-12 22:16:19 +0900

    add: fetch image, create imageModel instance, render inside listview

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,9 @@
 // import helper library
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'dart:convert' as convert;
+
+import 'package:flutter_pics/src/models/image_model.dart';
 
 class MyApp extends StatefulWidget {
   // add constructor
@@ -13,6 +17,21 @@ class MyApp extends StatefulWidget {
 // app level
 class _MyAppState extends State<MyApp> {
   int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    setState(() => counter++);
+    var url = Uri.https('jsonplaceholder.typicode.com', '/photos/$counter');
+    var response = await get(url);
+    if (response.statusCode == 200) {
+      var jsonResponse =
+          convert.jsonDecode(response.body) as Map<String, dynamic>;
+      print(jsonResponse);
+      setState(() => images.add(ImageModel.fromJson(jsonResponse)));
+    } else {
+      throw Exception('Failed to load image at $counter');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,16 +41,22 @@ class _MyAppState extends State<MyApp> {
           primaryColor: Colors.blueGrey,
         ),
         home: Scaffold(
-            body: Text('$counter'),
+            body: ListView.builder(
+              physics: const ClampingScrollPhysics(),
+              itemCount: images.length,
+              itemBuilder: (context, index) {
+                var url = images[index].url;
+                if (url == null) return const Text('No image');
+                return Image.network(url);
+              },
+            ),
             appBar: AppBar(
-              title: const Text('Hot reload works!'),
+              title: const Text('Flutter Pics'),
               backgroundColor: Colors.blueGrey[900],
             ),
             floatingActionButton: FloatingActionButton(
-                onPressed: () {
-                  this.counter += 1;
-                },
-                backgroundColor: Colors.greenAccent[400],
+                onPressed: fetchImage,
+                backgroundColor: Colors.pinkAccent,
                 child: const Icon(Icons.add))));
   }
 }
